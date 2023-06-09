ee0b61dce3de4408267406b4b6c31bc5dcbd725a
Vivian Rook <vrook@wikimedia.org>
2023-06-09 13:17:14 -0400

    column formatting

--- volume.tf
+++ volume.tf
@@ -7,5 +7,5 @@ resource "openstack_blockstorage_volume_v3" "volume" {
 
 resource "openstack_compute_volume_attach_v2" "va_1" {
   instance_id = "${resource.openstack_compute_instance_v2.vm.id}"
-  volume_id  = "${openstack_blockstorage_volume_v3.volume.id}"
+  volume_id   = "${openstack_blockstorage_volume_v3.volume.id}"
 }
