660f8af7e1d9e7acca36ed4e4578f199cd29d51e
Vivian Rook <vrook@wikimedia.org>
2023-06-09 13:51:21 -0400

    attach security group to VM

--- vm.tf
+++ vm.tf
@@ -7,6 +7,7 @@ resource "openstack_compute_instance_v2" "vm" {
   name            = "testlabs-tf"
   image_id        = data.openstack_images_image_v2.debian.id
   flavor_id       = "bb8bee7e-d8f9-460b-8344-74f745c139b9" # update to lookup?
+  security_groups = ["${openstack_compute_secgroup_v2.secgroup.name}"]
 
   network {
     name = "${var.fixed_subnet[var.datacenter]}"
