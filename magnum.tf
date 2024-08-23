f0ea67fef980424beff94e65bc82916e00deaaeb
Vivian Rook <vrook@wikimedia.org>
2024-08-23 12:44:36 -0400

    Newer fcos
    Bug: T373207

--- magnum.tf
+++ magnum.tf
@@ -37,7 +37,7 @@ variable "image_name" {
   type = map(any)
   default = {
     "codfw1dev" = "Fedora-CoreOS-34"
-    "eqiad1"    = "magnum-fedora-coreos-34"
+    "eqiad1"    = "Fedora-CoreOS-38"
   }
 }
 
@@ -64,10 +64,10 @@ resource "openstack_containerinfra_clustertemplate_v1" "template_126" {
   network_driver        = "flannel"
 
   labels = {
-    kube_tag                  = "v1.27.8-rancher2"
+    kube_tag                  = "v1.26.8-rancher1"
     container_runtime         = "containerd"
-    containerd_version        = "1.6.28"
-    containerd_tarball_sha256 = "f70736e52d61e5ad225f4fd21643b5ca1220013ab8b6c380434caeefb572da9b"
+    containerd_version        = "1.6.20"
+    containerd_tarball_sha256 = "1d86b534c7bba51b78a7eeb1b67dd2ac6c0edeb01c034cc5f590d5ccd824b416"
     hyperkube_prefix          = "docker.io/rancher/"
     cloud_provider_enabled    = "true"
   }
