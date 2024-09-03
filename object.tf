c480ceac037b437445e025c703e9576141015f51
Vivian Rook <vrook@wikimedia.org>
2024-09-03 15:02:40 -0400

    Add test for object storage
    Bug: T365820

--- object.tf
+++ object.tf
@@ -1,20 +1,10 @@
 resource "openstack_objectstorage_container_v1" "tf_container" {
-  region = var.region[var.datacenter]
   name   = "tofu-test-container"
-
-  metadata {
-    tofu = "soy"
-  }
 }
 
 resource "openstack_objectstorage_object_v1" "tofu_object" {
-  region         = "RegionOne"
   container_name = openstack_objectstorage_container_v1.tf_container.name
-  name           = "test/object.file"
-  metadata {
-    tofu = "soy"
-  }
+  name           = "tofu-test-directory/object.file"
 
-  content_type = "application/json"
   content      = "tofu test file"
 }
